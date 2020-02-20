a6b4cd60735b376c861c86eb1aa70f579c8862dd
Michael Verschoof <m.verschoof@btcdirect.eu>
2020-02-20 17:17:32 +0100

    Removed unused enums

--- src/lib/units/constants.ts
+++ src/lib/units/constants.ts
@@ -8,28 +8,3 @@ export enum PeriodType {
     SECOND = 'second',
     MILLISECOND = 'millisecond'
 }
-
-export enum Months {
-    JANUARY = 1,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER
-}
-
-export enum WeekDays {
-    MONDAY = 1,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
-}
